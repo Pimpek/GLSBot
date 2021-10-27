d81a017318951bc76258d3fda8cae3f4165f9752
Pimpek <51481138+Pimpek@users.noreply.github.com>
2021-10-27 22:32:00 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "ODY3ODA4OTA1MjQ4NTA1OTA3.YPmgBg.Q5x60LXosctCXICRGU2HLpPcMxw",
+    token: "ODY3ODA4OTA1MjQ4NTA1OTA3.YPmgBg.5N_4a2zAXJ4wq8Z5iKsA4RFh34k",
     prefix: "gls!",
 }
